40a0a6bede2b4598576d206cb17d26e1e061cdb0
mathêe <matheusfc2626@gmail.com>
2022-02-03 20:49:15 -0300

    commit

--- ex3.js
+++ ex3.js
@@ -1,11 +1,11 @@
-let numeros = [1,2,3,4,5]
+let numeros = [1,2,3,4,5,7,9,11]
 let numerosImpares = []
 let somaImpares = 0
 
-for (cont = 0; cont < 5; cont++) {
+for (cont = 0; cont < numeros.length; cont++) {
     if (numeros[cont]%2 != 0) {
         numerosImpares.push(numeros[cont])
-        somaImpares = somaImpares + numeros[cont]
+        somaImpares += numeros[cont]
     }
 }
 
